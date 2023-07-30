db7145aa9ee094ef1f7317cb2f7fabce5973788e
wdd <wdd296151852@163.com>
2023-07-30 17:47:45 +0800

    1

--- client/src/pages/admin/index.jsx
+++ client/src/pages/admin/index.jsx
@@ -1,6 +1,17 @@
 import React,{ useEffect, useRef } from "react"
 import './index.css'
-import Haibao from '../../asstes/images/fengmian/haibao.png'
+
+import Img1 from '../../asstes/images/chuchang/b1.png'
+import Img2 from '../../asstes/images/chuchang/b2.png'
+import Img3 from '../../asstes/images/chuchang/b3.png'
+import Img4 from '../../asstes/images/chuchang/b4.png'
+import Img5 from '../../asstes/images/chuchang/b5.png'
+import Img6 from '../../asstes/images/chuchang/g1.png'
+import Img7 from '../../asstes/images/chuchang/g2.png'
+import Img8 from '../../asstes/images/chuchang/g3.png'
+import Img9 from '../../asstes/images/chuchang/g4.png'
+import Img10 from '../../asstes/images/chuchang/g5.png'
+
 let timer = null;
 
 function Admin() {
@@ -135,7 +146,18 @@ function Admin() {
           <span>邪剑黑曜</span> 
         </div>
         <div className="msg two">
-          <img src={Haibao} alt="" />
+          <div>
+            <img className="img1 top" src={Img5} alt="" />
+            <img className="img2 bot" src={Img4} alt="" />
+            <img className="img3 top" src={Img3} alt="" />
+            <img className="img4 bot" src={Img2} alt="" />
+            <img className="img5 top" src={Img1} alt="" />
+            <img className="img6 bot" src={Img6} alt="" />
+            <img className="img7 top" src={Img7} alt="" />
+            <img className="img8 bot" src={Img8} alt="" />
+            <img className="img9 top" src={Img9} alt="" />
+            <img className="img10 bot" src={Img10} alt="" />
+          </div>
         </div>
       </div>
     </div>
